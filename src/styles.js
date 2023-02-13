2ae151eb36d5ae049731f82d8081f2d8d347c991
Stefano Marra <stefano.marra1987@gmail.com>
2023-02-13 18:55:38 +0100

    Add styles

---
+++ src/styles.js
@@ -0,0 +1,87 @@
+export const color = {
+  // Palette
+  primary: '#0062FF',
+  secondary: '#FFC542',
+  tertiary: '#92929D',
+
+  // Monochrome
+  lightest: '#FFFFFF',
+  lighter: '#FAFAFB',
+  light: '#F3F3F3',
+  mediumlight: '#EEEEEE',
+  medium: '#DDDDDD',
+  mediumdark: '#999999',
+  dark: '#666666',
+  darker: '#444444',
+  darkest: '#333333',
+
+  // Status
+  success: '#3dc763',
+  danger: '#FC5A5A',
+  warning: '#E69D00',
+};
+
+export const typography = {
+  type: {
+    primary: '"Roboto", sans-serif',
+    code: '"SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace',
+  },
+  weight: {
+    regular: 400,
+    medium: 500,
+    semibold: 600,
+    bold: 700,
+    extrabold: 800,
+    black: 900,
+  },
+  size: {
+    s1: 12,
+    s2: 14,
+    s3: 16,
+
+    m1: 18,
+    m2: 20,
+    m3: 22,
+
+    l1: 24,
+    l2: 28,
+    l3: 32,
+  },
+};
+
+export const spacing = {
+  padding: {
+    none: 0,
+    xsmall: 5,
+    small: 10,
+    medium: 20,
+    large: 30,
+  },
+  margin: {
+    none: 0,
+    xsmall: 5,
+    small: 10,
+    medium: 20,
+    large: 30,
+  },
+  borderRadius: {
+    none: 0,
+    xsmall: 2,
+    small: 4,
+    medium: 6,
+    large: 8,
+
+    default: 6,
+  },
+};
+
+export const responsive = {
+  columns: 12,
+  breakpoints: {
+    xs: 0,
+    sm: 576,
+    md: 768,
+    lg: 992,
+    xl: 1200,
+  },
+};
