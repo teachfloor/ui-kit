c2fcbb4070bba5dc664917de2fd243c754f979a7
Stefano Marra <stefano.marra1987@gmail.com>
2023-03-27 01:33:20 +0200

    Fix

--- src/styled/index.jsx
+++ src/styled/index.jsx
@@ -1,4 +1,4 @@
 import styled from '@emotion/styled'
-import { css } from '@emotion/react'
+import { css, keyframes } from '@emotion/react'
 
-export { styled, css }
+export { styled, css, keyframes }
